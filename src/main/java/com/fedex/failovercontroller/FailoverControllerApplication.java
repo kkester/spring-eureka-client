bbe3a4631a60f005c0713da1e9e7e8df647192dd
Keith Kester (OSV) <keith.kester.osv@fedex.com>
2022-05-12 18:20:57 -0400

    Initial Commit

---
+++ src/main/java/com/fedex/failovercontroller/FailoverControllerApplication.java
@@ -0,0 +1,13 @@
+package com.fedex.failovercontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FailoverControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FailoverControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fedex/failovercontroller/FailoverControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fedex.failovercontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FailoverControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
